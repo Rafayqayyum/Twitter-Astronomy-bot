1e416f474741a64715d1463e0f2be9115f2f5c8f
Rafay Qayyum <rafayqayyum786@gmail.com>
2023-05-25 02:13:00 +0500

    removed the auth on home route

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    secret_key=request.headers.get("Secret-Key")
-    if secret_key != os.environ.get('SECRET_KEY'):
-        return make_response('Invalid Secret Key', 401)
-    else:
-        return make_response('Hello, World!', 200)
+    return make_response('Hello, World!', 200)
     
 @app.route('/post_tweet_with_picture_of_the_day')
 def post_tweet_with_picture_of_the_day_route():
